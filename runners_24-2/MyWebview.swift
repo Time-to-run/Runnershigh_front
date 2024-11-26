f326a67ddc877b4ec38f43fe74fd5e212c230cdb
runners <20213860@office.deu.ac.kr>
2024-11-26 19:28:23 +0900

    뒤로가기 제스처 추가

--- runners_24-2/MyWebview.swift
+++ runners_24-2/MyWebview.swift
@@ -27,20 +27,20 @@ struct MyWebview: UIViewRepresentable {
         // 웹뷰 인스턴스 생성
         let webview = WKWebView()
         
+        // 스와이프 제스처로 뒤로가기 및 앞으로가기 활성화
+        webview.allowsBackForwardNavigationGestures = true
+        
         // 웹뷰를 로드한다.
         webview.load(URLRequest(url: url))
         
         return webview
-        
     }
     
     // 업데이트 ui view
     func updateUIView(_ uiView: WKWebView, context: UIViewRepresentableContext<MyWebview>) {
-           
-       }
-    
+        // 필요 시 업데이트 구현
+    }
 }
-
 
 struct MyWebview_Previews: PreviewProvider {
     static var previews: some View {
